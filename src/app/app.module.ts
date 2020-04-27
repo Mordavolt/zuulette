061cb83c10f148b79f996807bf84815fdae7f986
Arturs Drozdovs <arturs.drozdovs@teampicnic.com>
2020-04-28 00:07:53 +0200

    Initial version done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExerciseComponent } from './exercise-domain/exercise/exercise.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSliderModule } from '@angular/material/slider';
+import { TrainingMainComponent } from './training-main/training-main.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexModule } from '@angular/flex-layout';
+import { RoundComponent } from './round-domain/round/round.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExerciseComponent,
+    TrainingMainComponent,
+    RoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatSliderModule,
+    MatInputModule,
+    FormsModule,
+    MatExpansionModule,
+    MatButtonModule,
+    FlexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
